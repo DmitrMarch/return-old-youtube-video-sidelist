016e71b32badf396e49a22512d942a2a4728ee84
Dmitriy Marchenko <98233694+DmitrMarch@users.noreply.github.com>
2025-11-18 23:56:06 +0300

    add start user script

---
+++ script.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Return old YouTube video sidelist
+// @namespace    https://github.com/DmitrMarch/return-old-youtube-video-sidelist
+// @version      0.1
+// @description  Script for the return of the old sidelist design for YouTube videos
+// @author       DmitrMarch
+// @match        https://www.youtube.com/watch*
+// @icon         https://www.youtube.com/s/desktop/9c0f82da/img/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
